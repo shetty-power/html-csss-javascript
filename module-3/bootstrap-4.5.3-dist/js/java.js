7082fbd44add669fb0289ccb410dfb89b88d1c1c
shetty-power <shreyas@man.com>
2020-10-24 16:25:33 +0530

    save

---
+++ module-3/bootstrap-4.5.3-dist/js/java.js
@@ -0,0 +1,11 @@
+function open() {
+  var x;
+  x= document.getElementById("stuff");
+  x.class = "openme";
+}
+function close() {
+  var x;
+  x = document.getElementsById("stuff");
+  x.class = "closeme";
+
+}
