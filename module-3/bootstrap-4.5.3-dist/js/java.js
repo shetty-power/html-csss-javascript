cf8a1fd8246783a2bd772fa550df10aa1591c8c4
shetty-power <shreyas@man.com>
2020-10-24 16:40:48 +0530

    huh

--- module-3/bootstrap-4.5.3-dist/js/java.js
+++ module-3/bootstrap-4.5.3-dist/js/java.js
@@ -1,9 +1,9 @@
-function open() {
+function open(){
   var x;
   x= document.getElementById("stuff");
   x.class = "openme";
 }
-function close() {
+function close(){
   var x;
   x = document.getElementsById("stuff");
   x.class = "closeme";
