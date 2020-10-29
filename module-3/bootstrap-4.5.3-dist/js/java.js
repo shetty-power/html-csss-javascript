679218552effdea8bcf4d3a9f54b9ab04e260f66
shetty-power <shreyas@man.com>
2020-10-29 10:46:08 +0530

    comrno

--- module-3/bootstrap-4.5.3-dist/js/java.js
+++ module-3/bootstrap-4.5.3-dist/js/java.js
@@ -1,11 +1,19 @@
-function open(){
-  var x;
-  x= document.getElementById("stuff");
-  x.class = "openme";
-}
-function close(){
-  var x;
-  x = document.getElementsById("stuff");
-  x.class = "closeme";
+let image=document.getElementById('img');
+let hide=document.getElementById('hide');
+let show=document.getElementById('show');
+let text=document.getElementById('text');
 
+hide.onclick =function () {
+	image.style.display='none';
+	// body...
+}
+show.ondblclick =function () {
+	image.style.display='block';
+	// body...
+}
+text.onmouseover=function(){
+	text.innerHTML='oops';
+}
+text.onmouseoutr=function(){
+	text.innerHTML='MOUse';
 }
